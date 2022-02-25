f442ebf58758548234ea29316dc408f3a86ac3f4
ryan0000 <rlibed@esri.com>
2022-02-25 14:49:34 -0800

    Updated boilerplate to use calcite components react

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import "./Header.scss";
+
 interface HeaderProps {
   title: string;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+setAssetPath(window.location.href);
 import "./index.scss";
 import App from "./App";
 
-// Calcite Components
-import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist";
-import { applyPolyfills } from "@esri/calcite-components/dist/loader";
-
-import {
-  setAssetPath,
-  defineCustomElements
-} from "@esri/calcite-components/dist/custom-elements";
-
-(async function init() {
-  setAssetPath(window.location.href);
-
-  await applyPolyfills();
-  defineCustomElements(window);
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-})();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
